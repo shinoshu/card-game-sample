3e05ec3ec6fa60d4f96d009de0666a190617af6a
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-22 21:17:08 +0900

    update field.component

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../card';
 
 @Component({
   selector: 'app-field',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./field.component.scss']
 })
 export class FieldComponent implements OnInit {
+  imagePaths: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    for (let [key, value] of Object.entries(Card)) {
+      this.imagePaths.push(value);
+    }
   }
-
 }
