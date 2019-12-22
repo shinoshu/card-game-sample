be08c40d10cf652ac0d1c78c83eed846d6d2c791
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-23 00:01:40 +0900

    update field.component

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../card.enum';
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-field',
@@ -15,5 +16,8 @@ export class FieldComponent implements OnInit {
     for (const [key] of Object.entries(Card)) {
       this.cards.push(Card[key]);
     }
+
+    this.cards = lodash.shuffle(this.cards);
+    this.cards = this.cards.slice(0, 5);
   }
 }
