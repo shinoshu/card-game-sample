6b04ec3f501021df01a3c70cd1a4497e73e41042
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-22 19:50:37 +0900

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CardComponent } from './card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
