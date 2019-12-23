e4c4e6df4c459c015198943fa013075c1dfdb911
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-24 00:02:13 +0900

    update point-calculator.service.ts

--- src/app/point-calculator.service.ts
+++ src/app/point-calculator.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Card } from './card.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class PointCalculatorService {
 
   constructor() { }
+
+  calculate(cards: Card[]): number {
+    return 0;
+  }
 }
